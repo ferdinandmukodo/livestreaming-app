d348e51a9b591aab6fb37f08f5a7d798e6e87402
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-10-06 13:00:15 +0300

    debug errors in app and index js files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useContext, Fragment } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Store } from './Store';
+import LoadingScreen from './components/LoadingScreen';
+import Nav from './components/Nav';
+//import DownloadManager from './routes/DownloadManager';
+import Home from './routes/Home';
+import MediaBrowser from './routes/MediaBrowser';
+import MediaPlayer from './routes/MediaPlayer';
+//import Configuration from './routes/Configuration';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const routes = [
+  {
+    exact: true,
+    key: 'media',
+    label: 'Shows',
+    path: '/media',
+  },
+  {
+    exact: true,
+    key: 'downloads',
+    label: 'Downloads',
+    path: '/downloads',
+  },
+  {
+    exact: true,
+    key: 'configuration',
+    label: 'Favourite',
+    path: '/configuration',
+  },
+];
+
+const App = () => { 
+  const { state: { isInit, isSupported }, dispatch } = useContext(Store);
+
+  useEffect(() => {
+    !isInit && dispatch({ type: 'INIT', dispatch });
+  });
+
+  if (!isInit) return <LoadingScreen />;
+
+  if (!isSupported) return (
+    <div className="alert alert-danger" role="alert">
+      <h4 className="alert-heading">Browser not supported!</h4>
+      <hr />
+      Unfortunately your browser does not support all required technologies. Please update your browser. Safari on iOS is not supported, but Chrome and Firefox on iOS are.
     </div>
+  );
+
+  return (
+    <Router onUpdate={() => window.scrollTo(0, 0)}>
+      <div className="App">
+        <Nav routes={routes} />
+        <main className="container" style={{ paddingTop: '4.5rem' }}>
+          <Fragment>
+            <Route
+              component={Home}
+              exact
+              path="/"
+            />
+            <Route
+              component={MediaBrowser}
+              exact
+              path="/media"
+            />
+            {/* <Route
+              component={DownloadManager}
+              exact
+              path="/downloads"
+            /> */}
+            <Route
+              component={MediaPlayer}
+              exact
+              path="/:mode(stream|offline)/:id"
+            />
+            {/* <Route
+              component={Configuration}
+              exact
+              path="/configuration"
+            /> */}
+          </Fragment>
+        </main>
+      </div>
+    </Router>
   );
 }
 
